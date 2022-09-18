5dafc76d917af004301e6822b2cc56f336c1bfd9
dmgcodevil <dmgcodevil@gmail.com>
2022-09-18 15:14:36 -0400

    removed Handler struct

--- src/Craze.jl
+++ src/Craze.jl
@@ -9,17 +9,13 @@ abstract type ControlEvent <: Event end
 struct Start <: ControlEvent end
 struct Stop <: ControlEvent end
 
-struct Handler{T,S} end
-
 mutable struct Process{T<:Event,S}
     state::S
-    handler::Handler{Union{T,ControlEvent},S}
     chan::Channel{Union{T,ControlEvent}}
     @atomic started::Bool
     @atomic stopped::Bool
     Process{T,S}(state::S) where {T<:Event,S} =
         new(state,
-            Handler{Union{T,ControlEvent},S}(),
             Channel{Union{T,ControlEvent}}(1),
             false,
             false)
@@ -41,36 +37,40 @@ function start(p::Process{T,S}) where {T,S}
     end
     @async while true
         event = take!(p.chan)
-        if isa(event, Stop) && hasmethod(p.handler, Tuple{Stop,S})
+        if isa(event, Stop) && hasmethod(p, Tuple{Stop,S})
             (_, stopped) = @atomicreplace p.stopped false => true
             if stopped
-                p.handler(event, p.state)
+                p(event, p.state)
             else
                 error("process is already stoped")
             end
             break
-        elseif isa(event, Start) && hasmethod(p.handler, Tuple{Start,S})
-            p.handler(event, p.state)
+        elseif isa(event, Start) && hasmethod(p, Tuple{Start,S})
+            p(event, p.state)
         else
-            p.handler(event, p.state)
+            p(event, p.state)
         end
+    @info "event loop has been terminated"
     end
 end
 
 function handle(p::Process{T,S}, event::T) where {T,S}
-    p.handler(event, p.state)
+    p(event, p.state)
 end
 
 # Syntax
 macro handler(ex::Expr)
-    inputType = ex.args[1].args[1].args[1]
-    eventType = ex.args[1].args[2].args[2]
-    stateType = ex.args[1].args[3].args[2]
-    eventVar = Symbol(ex.args[1].args[2].args[1])
-    stateVar = Symbol(ex.args[1].args[3].args[1])
+    # dump(ex)
+    def = ex.args[1]
     body = ex.args[2]
+    inputType = def.args[1].args[2]
+    eventType = def.args[2].args[1].args[2]
+    stateType = def.args[2].args[2].args[2]
+    eventVar = Symbol(def.args[2].args[1].args[1])
+    stateVar = Symbol(def.args[2].args[2].args[1])
+
     quote
-        function (::Handler{Union{$__module__.$inputType,ControlEvent},$__module__.$stateType})($eventVar::$__module__.$eventType, $stateVar::$__module__.$stateType)
+        function (::Process{$__module__.$inputType,$__module__.$stateType})($eventVar::$__module__.$eventType, $stateVar::$__module__.$stateType)
             $body
         end
     end
--- src/main.jl
+++ src/main.jl
@@ -21,15 +21,15 @@ end
 Input = Union{Foo,Bar}
 
 # Handlers
-@handler {Input}(event::Bar, state::State) = println("handle: ", event, ", state: ", state)
-@handler {Input}(e::Foo, s::State) =
+@handler Process{Input}::(event::Bar, state::State) = println("handle: ", event, ", state: ", state)
+@handler Process{Input}::(e::Foo, s::State) =
     begin
         println("handle: ", e, " start")
         println("handle: ", e, " end")
     end
 
-@handler {Input}(event::Start, state::State) = println("process has been started")
-@handler {Input}(event::Stop, state::State) = println("process has been stopped")
+@handler Process{Input}::(event::Start, state::State) = println("process has been started")
+@handler Process{Input}::(event::Stop, state::State) = println("process has been stopped")
 
 p = Process{Input,State}(State(0))
 
